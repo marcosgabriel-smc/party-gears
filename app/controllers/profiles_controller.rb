575e2782c8b171539093d9dfc35ce3a1ba28c19e
Marcos Costa <marcosgabriel.smc@gmail.com>
2023-05-31 12:19:54 -0300

    Changes in the route to your profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,19 @@
 class ProfilesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[show]
+  before_action :set_user
+
   def my_profile
-    @user = current_user
   end
 
   def show
     @gear = Gear.find(params[:gear_id])
-    @user = User.find(@gear.user_id)
+    @owner = @gear.user
   end
+
+  private
+
+  def set_user
+    @user = current_user if user_signed_in?
+  end
+
 end
