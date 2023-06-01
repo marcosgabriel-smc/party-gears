ebe7863ea08281ccd2c9c20af3bbae9ca26dec87
Marcos Costa <marcosgabriel.smc@gmail.com>
2023-06-01 16:34:13 -0300

    Some changes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,10 @@ class ProfilesController < ApplicationController
   before_action :set_user
 
   def my_profile
-    @my_offers = Offer.where(user: @user)
+    ## Assingning different values so the sorts puts the true/false  in an order
+    @my_gears = Gear.where(user: @user)
+    @reservations = @user.offers.sort_by{|offer| offer.start_date}
+    @my_offers = Offer.joins(gear: :user).where(users: { id: current_user.id })
   end
 
   def show
@@ -16,5 +19,4 @@ class ProfilesController < ApplicationController
   def set_user
     @user = current_user if user_signed_in?
   end
-
 end
